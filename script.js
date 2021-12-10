84a116e7e9f100823185fb8885f48c66230c7eb5
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-10 15:50:10 -0300

    requisito 1 concluido

---
+++ script.js
@@ -0,0 +1,8 @@
+// Requisito 1
+
+let body = document.querySelector("body");
+let header = document.createElement('header');
+let h1 = document.createElement('h1');
+h1.innerText = 'Minha Lista de Tarefas';
+body.appendChild(header);
+header.appendChild(h1);
